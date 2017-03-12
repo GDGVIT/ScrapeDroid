9a74e115d7ba4f1b5178c5b9c82d6dc970da777d
Ramkishorevit <ramkishorevit@gmail.com>
2017-03-13 00:41:08 +0530

    basic response check

--- app/src/main/java/com/gdgvitvellore/scrapdroid/ScrapperActivity.java
+++ app/src/main/java/com/gdgvitvellore/scrapdroid/ScrapperActivity.java
@@ -2,12 +2,17 @@ package com.gdgvitvellore.scrapdroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.gdgvitvellore.scrapdroidlibrary.ScrapDroid;
 
 public class ScrapperActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrapper);
+        Log.v("response_view",new ScrapDroid().getResponse());
     }
 }
--- scrapdroidlibrary/src/main/java/com/gdgvitvellore/scrapdroidlibrary/ScrapDroid.java
+++ scrapdroidlibrary/src/main/java/com/gdgvitvellore/scrapdroidlibrary/ScrapDroid.java
@@ -1,10 +1,115 @@
 package com.gdgvitvellore.scrapdroidlibrary;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
+
 /**
  * Created by ramkishorevs on 12/03/17.
  */
 
 public class ScrapDroid {
 
-   
+
+    public String getResponse()
+    {
+        String response = null;
+
+        try {
+          response =new FetchResponse().execute().get();
+          Log.v("response",response);
+        }
+        catch (InterruptedException e) {
+
+           e.printStackTrace();
+
+        }
+        catch (ExecutionException e) {
+
+            e.printStackTrace();
+        }
+
+        return response;
+
+    }
+
+
+
+    private class FetchResponse extends AsyncTask<Void, Void, String> {
+
+        @Override
+        protected String doInBackground(Void... params) {
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String response = null;
+
+            try {
+
+                URL url = new URL("http://gdgvitvellore.com/");
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                response = buffer.toString();
+                Log.v("response",response);
+
+                return response;
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                    }
+                }
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.v("response",s);
+
+
+        }
+    }
 }
+
